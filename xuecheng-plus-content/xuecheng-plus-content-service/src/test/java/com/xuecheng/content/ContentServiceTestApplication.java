711709134de980b75b3aa7e8390254bc6f6b49d0
Kevin <hello@itcast.cn>
2025-08-09 17:20:48 +0800

    课程发布(实现课程搜索)业务代码

--- xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/ContentServiceTestApplication.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/test/java/com/xuecheng/content/ContentServiceTestApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
  * 它只在测试期间生效，用于加载测试所需的环境。
  */
 @SpringBootApplication
-// 关键：扫描 Feign 客户端，路径现在是 service 模块自己的包
+@EnableFeignClients(basePackages = {"com.xuecheng.content.feignclient"})
 @ComponentScan(basePackages = {"com.xuecheng.content", "com.xuecheng.messagesdk"})
 public class ContentServiceTestApplication {
     // 这个类不需要 main 方法
